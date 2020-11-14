05021426878abe10543e8c7a1f1907fad663db84
ddouteaux <damien.douteaux@gmail.com>
2020-11-14 21:33:47 +0100

    feat: Pages d'aide

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -6,6 +6,16 @@ module.exports = function(app, baseDir) {
         res.render('index');
     })
 
+    app.get('/help/plan', (req, res) => {
+        logger.route("GET /help/plan");
+        res.render("partials/help/plan_site");
+    })
+
+    app.get('/help/', (req, res) => {
+        logger.route("GET /help/");
+        res.render("partials/help/main");
+    })
+
     app.get('/*', (req, res) => {
         logger.route("* Default redirect");
         res.redirect('/?error=La page demandée n\'existe pas.');
